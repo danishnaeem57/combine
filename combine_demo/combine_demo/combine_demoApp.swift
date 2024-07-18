d84dc03c1d347b2ef514320cd08a3a5f5aabf58e
New Danish <danishn@joblogic.com>
2024-07-18 22:51:32 +0500

    initial commit

---
+++ combine_demo/combine_demo/combine_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  combine_demoApp.swift
+//  combine_demo
+//
+//  Created by New Danish on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct combine_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
